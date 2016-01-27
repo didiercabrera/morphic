c38e512d8d43d3c2b0cc72ff0ecd46cf3527e5c9
Didier Cabrera <didierctorres@gmail.com>
2016-01-26 21:01:29 -0600

    adding segmentation functions

--- processing/scalers.py
+++ processing/scalers.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import numpy as np
 
 def normalize(x,max):
 	return float(x/max)
@@ -6,8 +7,8 @@ def normalize(x,max):
 def denormalize(x,max):
 	return float(x*max)
 
-def rescale( data_set ,smax ,smin ):
-	return [ (x-smin)/(smax - smin) for x in data_set]
+def rescale( X ,smax ,smin ):
+	return [ (x-smin)/(smax - smin) for x in X]
 
-def unit_rescale( data_set ):
-	return [ (x/len(data_set)) for x in data_set ]
+def unit_rescale( X ):
+	return [ x/len(X) for x in X ]
--- processing/segmentation.py
+++ processing/segmentation.py
@@ -1,27 +1,56 @@
-'''
+import collections
+from itertools import groupby
+
+N = [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5]
+
+"""
+
 Segmentation of data by multiple algorithms,features or properties
 
+"""
 
-'''
+def by_bars( C , n_beats=16, beat_axis=4 ):
+	"""
+	Divide a composition C in bars with N beats,
 
+	Parameters
+	----------
+	C : 2D array
 
+	n_beats: int , optional (16 by default) length of the bar in beats
 
+	beat_axis : int , optional(4 by default) index of the beat value,
+				we assume the data has a regular event structure [I,P,V,D,S,E]
 
-def by_beats( data, n_beats=16 ):
-	'''
-	Split a track in chunks of length = n_beats
-	'''
-	chunks = []
+	"""
+	if not isinstance(C[0], collections.Sequence):
+		raise ValueError( "Events must be a Sequence" )
+	if len(C[0])>=beat_axis:
+		raise ValueError( "beat_axis is out of range" )
+
+	bars = []
 	cbeat = 0
 	for e in data:
-		I,P,V,D,S,E  = e
-		cb, s  = divmod(S,n_beats)
+		cb, s  = divmod( e[beat_axis] , n_beats )
 		if cb!=cbeat:
-			chunks.append([])
+			bars.append([])
 			cbeat = cb
-		chunks[int(cbeat-1)].append( [I,P,V,D,S%n_beats,E%n_beats] )
-	return chunks
+		bars[int(cbeat-1)].append( [I,P,V,D,S%n_beats,E%n_beats] )
+	return bars
 
+def by_axis( C, axis=0 ):
+	"""
+	Divide a composition C in subsets which contain one same property
 
-def by_instrument():
-	pass
+	Parameters
+	----------
+
+	C : 2D array
+
+	axis : int, optional( 0 by default) index of the property to be grouped by
+
+	"""
+	keyfunc = lambda x:x[axis]
+	C = sorted(C, key=keyfunc)
+	return [list(j) for i, j in groupby(C,keyfunc)]
+
